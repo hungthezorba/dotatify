87c6a99a187e00784de196eee47ee8a93b45162d
hungthezorba <hungthezorba@gmail.com>
2020-10-22 06:55:54 +0700

    Refactor matchInfo array

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ r = requests.get('https://liquipedia.net/dota2/Liquipedia:Upcoming_and_ongoing_m
 soup = BeautifulSoup(r.content)
 tables = soup.find_all('table', {'class' : 'wikitable wikitable-striped infobox_matches_content'})
 match = tables[0]
+matchInfo = []
+index = 0
 for child in tables:
 	if ('496' in child.findChild().text):
-		matchInfo = child.findChild().text.split('\n')
-		for element in matchInfo:
+		matchInfo.append(child.findChild().text.split('\n'))
+		for element in matchInfo[index]:
 			if element == '':
-				matchInfo.remove('');
-		print (matchInfo)
+				matchInfo[index].remove('');
+		index+=1
+print(matchInfo)
