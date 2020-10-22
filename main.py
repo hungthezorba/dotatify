f7e29f58338529b8156d311b05644201c594f0a4
hungthezorba <hungthezorba@gmail.com>
2020-10-22 07:13:33 +0700

    Refactor and clean format for matches list

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from bs4 import BeautifulSoup
 r = requests.get('https://liquipedia.net/dota2/Liquipedia:Upcoming_and_ongoing_matches')
 soup = BeautifulSoup(r.content)
 tables = soup.find_all('table', {'class' : 'wikitable wikitable-striped infobox_matches_content'})
-match = tables[0]
-matchInfo = []
+
+matches = []
 index = 0
 for child in tables:
-	if ('496' in child.findChild().text):
-		matchInfo.append(child.findChild().text.split('\n'))
-		for element in matchInfo[index]:
-			if element == '':
-				matchInfo[index].remove('');
+	if ('496' in child.findChild().text and 'vs' in child.findChild().text): #Retrieving upcoming match
+		matches.append(child.findChild().text.split('\n'))
+		matches[index] = [elem.strip() for elem in matches[index] if elem != '']
 		index+=1
-print(matchInfo)
+		
+print(matches)
