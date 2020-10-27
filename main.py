79b5661395156e2e7949f28359ee2820bf49acb7
hungthezorba <hungthezorba@gmail.com>
2020-10-27 13:24:54 +0700

    Reformat matchData

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ def main():
 		'teamLeft': '',
 		'teamRight': '',
 		'bestOf': '',
-		'time': '',
+		'schedule': {
+			'month': '',
+			'day': '',
+			'year': '',
+			'time': ''
+		},
 		'tournament': ''
 	}
 
@@ -30,19 +35,23 @@ def main():
 		matchData['teamLeft'] = match.find_all('a')[0].text
 		matchData['teamRight'] = match.find_all('a')[3].text
 		matchData['bestOf'] = match.find_all('td')[1].text.replace('\n','')
-		time = cleaningArray(match.find_all('span', {'class' : 'timer-object'})[0].contents[0].split(' '))
-		processedTime = (datetime.strptime(time[0] + ' ' + time[1] + ' ' +  time[2] + ' ' + time[3], '%B %d %Y %H:%M') + timedelta(hours=7))
-		processedTime = "{:d} {:d} {:d} {:d}:{:02d}".format(processedTime.month, processedTime.day, processedTime.year, processedTime.hour, processedTime.minute)
-		matchData['time'] = processedTime
+		schedule = cleaningArray(match.find_all('span', {'class' : 'timer-object'})[0].contents[0].split(' '))
+		processedSchedule = (datetime.strptime(schedule[0] + ' ' + schedule[1] + ' ' +  schedule[2] + ' ' + schedule[3], '%B %d %Y %H:%M') + timedelta(hours=7))
+		# processedSchedule = "{:d} {:d} {:d} {:d}:{:02d}".format(processedSchedule.month, processedSchedule.day, processedSchedule.year, processedSchedule.hour, processedSchedule.minute)
+		matchData['schedule']['month'] = processedSchedule.month
+		matchData['schedule']['day'] = processedSchedule.day
+		matchData['schedule']['year'] = processedSchedule.year
+		matchData['schedule']['time'] = "{:d}:{:02d}".format(processedSchedule.hour, processedSchedule.minute)
 		matchData['tournament'] = match.find_all('a')[5].text
-		matches.append(copy.copy(matchData))
-	
+		matches.append(copy.deepcopy(matchData))
+			
 	nextMatch = matches[0]
+	schedule = str(nextMatch['schedule']['month']) + ' ' + str(nextMatch['schedule']['day']) + ' ' + str(nextMatch['schedule']['year']) + ' ' + str(nextMatch['schedule']['time'])
 	#Notify a match in upcoming 1 hour
-	if (datetime.now() + timedelta(hours=2) > datetime.strptime(nextMatch['time'], '%m %d %Y %H:%M')): 
-		title = "Incoming 496 match"
-		message = nextMatch['teamLeft'] + ' ' + nextMatch['bestOf'] + ' ' +  nextMatch['teamRight'] + ' at ' + nextMatch['time'] + '\n' + nextMatch['tournament']
-		toaster.show_toast(title, message, duration=5)
+	# if (datetime.now() + timedelta(hours=1) > datetime.strptime(schedule, '%m %d %Y %H:%M')): 
+	title = "Incoming 496 match"
+	message = nextMatch['teamLeft'] + ' ' + nextMatch['bestOf'] + ' ' +  nextMatch['teamRight'] + ' at ' + nextMatch['schedule']['time'] + '\n' + nextMatch['tournament']
+	toaster.show_toast(title, message, duration=5, threaded=None)
 
 def cleaningArray(array):
 	removetable = str.maketrans('', '', '!@#$%^&*()-_=+[]{}"\'/?.,<>\\|')
